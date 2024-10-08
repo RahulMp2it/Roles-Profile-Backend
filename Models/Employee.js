f6f1f4022d75d45e9c7adba03e86d1e257ad8a30
Rahulmp2it <rahulsingh@mpsquare.in>
2024-10-08 18:17:56 +0530

    employee field

--- Models/Employee.js
+++ Models/Employee.js
@@ -3,10 +3,19 @@ import mongoose, { Model, Types } from "mongoose";
 //Defining Schema
 const employeeSchema = new mongoose.Schema({
   name: {type: String, required: true, trim: true},
-  profile: {type: String, required: true, trim: true},
-  department: {type: String, required: true, trim: true},
-  designation: {type: String, required: true, trim: true},
- 
+  email: { 
+    type: String, 
+    required: true, 
+    trim: true, 
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'], // Regex for email validation
+  },
+  phone: { 
+    type: String, 
+    required: true, 
+    trim: true, 
+    match: [/^\d{10}$/, 'Please enter a valid 10-digit phone number'], // Regex for phone number validation
+  }, 
 })
 
 // model
