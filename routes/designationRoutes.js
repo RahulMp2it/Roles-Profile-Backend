48ac7667d725633a30cb516ba9e8bcef684b0155
Rahulmp2it <rahulsingh@mpsquare.in>
2024-10-16 18:24:10 +0530

    routes change

--- routes/designationRoutes.js
+++ routes/designationRoutes.js
@@ -4,7 +4,7 @@ import DesignationController from '../Controllers/DesignationController.js';
 const designationRouter = express.Router();
 
 designationRouter.post('/', DesignationController.createDesignation);
-designationRouter.get('/designation/:id', DesignationController.getDesignation);
+designationRouter.get('/:id', DesignationController.getDesignation);
 designationRouter.get('/', DesignationController.getAllDesignations);
 designationRouter.put('/:id', DesignationController.updateDesignation);
 designationRouter.delete('/:id', DesignationController.deleteDesignation);
