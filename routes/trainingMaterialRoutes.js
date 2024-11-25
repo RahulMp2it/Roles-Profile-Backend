8f24f461c88bb7cb52c1c5381bc8e08b7a3852b3
RahulMp2it <rahulsingh@mpsquare.in>
2024-11-25 18:10:59 +0530

    new

--- routes/trainingMaterialRoutes.js
+++ routes/trainingMaterialRoutes.js
@@ -10,19 +10,19 @@ import upload from "../middlewares/multerConfig.js"
 
 const trainingRouter = express.Router();
 
-// POST /api/training-materials - Create a new training material
+// Create a new training material
 trainingRouter.post('/', upload.single("document"), createTrainingMaterial);
 
-// GET /api/training-materials - Get all training materials
+// Get all training materials
 trainingRouter.get('/', getAllTrainingMaterials);
 
-// GET /api/training-materials/:id - Get a training material by ID
+// Get a training material by ID
 trainingRouter.get('/:id', getTrainingMaterialById);
 
-// PUT /api/training-materials/:id - Update a training material by ID
+// Update a training material by ID
 trainingRouter.put('/:id', updateTrainingMaterial);
 
-// DELETE /api/training-materials/:id - Delete a training material by ID
+// Delete a training material by ID
 trainingRouter.delete('/:id', deleteTrainingMaterial);
 
 export default trainingRouter;
