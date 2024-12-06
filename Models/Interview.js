32460a2b746a4814cbda564d62ac8a631cda596f
RahulMp2it <rahulsingh@mpsquare.in>
2024-12-06 18:26:08 +0530

    add id false and one object

--- Models/Interview.js
+++ Models/Interview.js
@@ -1,19 +1,28 @@
 import mongoose from "mongoose";
 
+const StageSchema = new mongoose.Schema(
+  {
+    stage: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: Number,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
 // Defining Schema
 const interviewSchema = mongoose.Schema({
-  stages: [
-    {
-      stage: { type: String, required: true },
-      time: { type: Number, required: true }, // Time in minutes
-    },
-  ],
+  stages: [StageSchema], // Array of objects with stage and time
   profile: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Profile',
     required:true
   }
-})
+}) 
 
 const iterviewModal = mongoose.model("Interview", interviewSchema);
 
